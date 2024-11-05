735e4407fb8f674125f6f800117c223ff0cbb2f3
matheus <matheusfonseca5100@gmail.com>
2024-11-04 21:54:45 -0300

    feat: finalizacao do exercicio

---
+++ Atividade-testes/src/models/store.py
@@ -0,0 +1,26 @@
+from src.models.user import User
+
+
+class Store:
+
+    def __init__(self):
+        self.bd = [
+            User('Gabriel', 'Tester'),
+            User('Beatriz', 'Developer'),
+            User('Felipe', 'IT Specialist'),
+            User('Juliana', 'QA'),
+            User('Luana', 'Systems Analyst'),
+            User('Eduardo', 'Software Developer'),
+            User('Ricardo', 'Technical Support'),
+            User('Isabela', 'Project Manager'),
+            User('Thiago', 'DevOps'),
+            User('Carolina', 'Designer'),
+            User('André', 'Software Architect'),
+            User('Patrícia', 'Product Owner'),
+            User('Marcelo', 'Cybersecurity Specialist'),
+            User('Sofia', 'Business Analyst'),
+            User('João', 'Data Scientist'),
+            User('Mariana', 'Front-end Developer')
+
+        ]
+
